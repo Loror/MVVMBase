f09a8b9799bee0a344f026baf429181c8249eb8f
chen ran <lorrer@126.com>
2021-10-27 15:22:16 +0800

    新增图片加载

--- app/src/main/java/com/loror/mvvmbase/widget/Binding.kt
+++ app/src/main/java/com/loror/mvvmbase/widget/Binding.kt
@@ -5,10 +5,21 @@ import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
 import com.loror.lororUtil.image.ImageUtil
 
+/**
+ * url 网络地址 place 占位图 error 错误占位图
+ * */
 class Image(var url: String?, @DrawableRes var place: Int = 0, @DrawableRes var error: Int = 0)
 
 fun String.toImage(): Image {
     return Image(this)
+}
+
+fun String.toImage(place: Int): Image {
+    return Image(this, place)
+}
+
+fun String.toImage(place: Int, error: Int): Image {
+    return Image(this, place, error)
 }
 
 @BindingAdapter("imageUrl")
