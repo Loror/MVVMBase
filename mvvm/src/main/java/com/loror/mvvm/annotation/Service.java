a82d2455f0d64f9606c74a5771446a02c2706940
chen ran <lorrer@126.com>
2021-12-24 16:38:27 +0800

    修改注释

--- mvvm/src/main/java/com/loror/mvvm/annotation/Service.java
+++ mvvm/src/main/java/com/loror/mvvm/annotation/Service.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * 配置 若为interface且包含@BaseUrl注解将生成api 若为object将尝试实例化
+ * 配置 若为interface且包含value将尝试实例化value、若不包含value将生成api 若为object将尝试实例化
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
